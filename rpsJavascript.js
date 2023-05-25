bd6cf1832dc2ea2c06d74171af9d79bc20b0eb5d
Max <89766417+moltersd@users.noreply.github.com>
2023-05-25 15:03:11 -0400

    finalized the game

--- rpsJavascript.js
+++ rpsJavascript.js
@@ -8,17 +8,14 @@
 function getPlayerChoice(){
     let input = prompt("Please choose rock, paper, or scissors");
     choice = input;
-    console.log(choice)
     if((choice !="paper") && (choice !="scissors" ) && (choice != "rock")){
         console.log(`Incorrect selection. Please choose rock, paper, or scissors`)
         getPlayerChoice();
     }
-    
-    else { console.log(`You chose: ${choice}`)
-            return choice};
+    else {console.log(`You chose ${choice}`)
+        return choice};
         
 }
-getPlayerChoice();
 
 //Get computer Selection
 function getComputerChoice() {
@@ -36,7 +33,6 @@ function getComputerChoice() {
     return computerChoice;
 
 }
-getComputerChoice();
 
 
   //  Generate random number between 0 and 2
@@ -54,24 +50,70 @@ getComputerChoice();
     //  if player = rock & computer = scissors, player wins
   // if player = paper and computer = scissors, computer wins
   // if player = scissors and computer = scissors, tie
+  function playRound(playerSelection,computerChoice) {
+
+ 
   if (
-        ((computerChoice === "paper") && (choice === "paper")) || 
-        ((computerChoice === "rock") && (choice === "rock")) || 
-        ((computerChoice === "scissors") && (choice === "scissors"))
-        ) { console.log("Tie!")
+        ((computerChoice === "paper") && (playerSelection === "paper")) || 
+        ((computerChoice === "rock") && (playerSelection === "rock")) || 
+        ((computerChoice === "scissors") && (playerSelection === "scissors"))
+        ) { return ("Tie!")
 
   }
   else if (
-    ((computerChoice === "rock") && (choice === "paper")) || 
-    ((computerChoice === "paper") && (choice === "scissors")) || 
-    ((computerChoice === "scissors") && (choice === "rock"))
-    ) { console.log("Player Wins!!")
+    ((computerChoice === "rock") && (playerSelection === "paper")) || 
+    ((computerChoice === "paper") && (playerSelection === "scissors")) || 
+    ((computerChoice === "scissors") && (playerSelection === "rock"))
+    ) { return ("Player Wins!!")
     }
     else if (
-        ((computerChoice === "paper") && (choice === "rock")) || 
-        ((computerChoice === "scissors") && (choice === "paper")) || 
-        ((computerChoice === "rock") && (choice === "scissors"))
-        ) { console.log("Computer Wins!!")
+        ((computerChoice === "paper") && (playerSelection === "rock")) || 
+        ((computerChoice === "scissors") && (playerSelection === "paper")) || 
+        ((computerChoice === "rock") && (playerSelection === "scissors"))
+        ) { return("Computer Wins!!")
         }
+    }
 //output winner
 //console log
+
+/* Game
+    Play 5 rounds
+    Tally wins and losses for each player
+    Announce score at the end of each game
+    when someone wins 5 games, declare them the winner
+*/
+function game(bestOf) {
+    compCounter = 0;
+    playerCounter = 0
+    for (let i = 0 ; i< bestOf; i++){
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        text = playRound(playerSelection, computerSelection);
+        console.log(text)
+        if(text === "Computer Wins!!" ) {
+            compCounter = compCounter + 1
+        }
+        else if(text === "Player Wins!!" ) {
+            playerCounter = playerCounter + 1
+        }
+        console.log(`Current Score: Player: ${playerCounter}; Computer: ${compCounter}`)
+        
+    }
+    if(compCounter > playerCounter){
+        console.log(`Unfortunately the computer wins the game!`)
+    }
+    else if(compCounter < playerCounter){
+        console.log(`Yay, the player wins the game!`)
+    }
+    else if (compCounter = playerCounter){
+        console.log(`It was a tie!`)
+    }
+}
+game(7);
+
+
+
+
+
+
+
